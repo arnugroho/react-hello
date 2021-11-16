3e370affe1fad50c0e14b566313f5c2738fa1760
arnugroho <nugroho.akbar.r@gmail.com>
2021-11-16 19:29:30 +0700

    class component Hello World

---
+++ src/container/ContainerHello.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class ContainerHello extends React.Component {
+    render() {
+        return (
+            <h1>Hello Class Container</h1>
+        )
+    }
+}
